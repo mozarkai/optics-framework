1b5a627971808b90d6bedbc09b6bfef8b26a4909
Dhruv Menon <dhruvmenon1104@gmail.com>
2025-04-01 01:45:35 +0530

    style: integrate pydantic for data validation

--- optics_framework/common/text_interface.py
+++ optics_framework/common/text_interface.py
@@ -14,7 +14,7 @@ class TextInterface(ABC):
     and reference data as needed.
     """
 
-    def element_exist(self, input_data, reference_data) -> Optional[Tuple[int, int]]:
+    def element_exist(self, input_data: Any, reference_data: Any) -> Optional[Tuple[int, int]]:
         """
         Find the location of a reference image within the input data.
 
